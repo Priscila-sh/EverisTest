b60112ae03b9904d3fb37e8d7f2e266420397a39
PriscilaScharneski <priscila.scharneski@equiplano.com.br>
2020-12-11 16:45:05 -0300

    CI Selenium

--- src/test/java/everis/carrinho/utilitarios/BaseSelenium.java
+++ src/test/java/everis/carrinho/utilitarios/BaseSelenium.java
@@ -6,35 +6,26 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 
-import io.github.bonigarcia.wdm.WebDriverManager;
-
 public class BaseSelenium {
 
 	private static WebDriver driver = null;
-	// public static final String CHROME_DRIVER = "src/test/resources/chromedriver.exe";
+	public static final String CHROME_DRIVER = "src/test/resources/chromedriver.exe";
 	public static final boolean HEADLESS = false;
 	public static final String LOJA = "http://automationpractice.com/index.php";
 
 	@BeforeClass
 	public static void inicializarSistema() {
-		WebDriverManager.chromedriver().setup();
+		System.setProperty("webdriver.chrome.driver", CHROME_DRIVER);
 
-		ChromeOptions options = new ChromeOptions();
-		options.addArguments("--no-sandbox");
-		options.addArguments("--disable-dev-shm-usage");
-		options.addArguments("--headless");
+		if (HEADLESS) {
 
-		driver = new ChromeDriver(options);
+			driver = iniciarWebDriverEmModoHeadless();
+		} else {
 
-		// if (HEADLESS) {
-		//
-		// driver = iniciarWebDriverEmModoHeadless();
-		// } else {
-		//
-		// driver = iniciarWebDriverEmModoDefault();
-		// }
+			driver = iniciarWebDriverEmModoDefault();
+		}
 
-		// driver.manage().window().maximize();
+		driver.manage().window().maximize();
 		driver.get(LOJA);
 
 	}
@@ -44,15 +35,15 @@ public class BaseSelenium {
 		driver.quit();
 	}
 
-	// private static ChromeDriver iniciarWebDriverEmModoDefault() {
-	// return new ChromeDriver();
-	// }
-	//
-	// private static ChromeDriver iniciarWebDriverEmModoHeadless() {
-	// ChromeOptions options = new ChromeOptions();
-	// options.addArguments("--headless");
-	// return new ChromeDriver(options);
-	// }
+	private static ChromeDriver iniciarWebDriverEmModoDefault() {
+		return new ChromeDriver();
+	}
+
+	private static ChromeDriver iniciarWebDriverEmModoHeadless() {
+		ChromeOptions options = new ChromeOptions();
+		options.addArguments("--headless");
+		return new ChromeDriver(options);
+	}
 
 	public static WebDriver getDriver() {
 		return driver;
